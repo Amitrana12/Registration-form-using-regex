afbdb916a4d69de0d0e977bad3c6de50335a9e46
amit <[amirana14325@gmail.com]>
2021-03-14 00:42:06 +0530

    [Amit Rana ]Added::Welcome massege

---
+++ registration regex/registration regex/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace registration_regex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my registration form regex apllication by Amit Rana");
+        }
+    }
+}
