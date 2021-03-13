6394ec1a8b99d4cdb379fae6469eaa8b15c161de
amit <[amirana14325@gmail.com]>
2021-03-14 02:53:15 +0530

    [Amit Rana]Added::MS unit test for Email mathod

--- registration regex/Registrationtest/UnitTest1.cs
+++ registration regex/Registrationtest/UnitTest1.cs
@@ -30,5 +30,14 @@ namespace Registrationtest
             //Assert
             Assert.IsTrue(result);
         }
+        [TestMethod]
+        public void Given_EmailId_WhenValidate_ShouldReturn_True()
+        {
+            string Email = "amirana14325@gmail.com";
+            //Act
+            bool result = person.ValidateEmail(Email);
+            //Assert
+            Assert.IsTrue(result);
+        }
     }
 }
