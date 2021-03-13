9ca1c2cac3b7b91eecb2920ba8f1a88ca40fcbec
amit <[amirana14325@gmail.com]>
2021-03-14 00:50:22 +0530

    [Amit Rana ]Added::adding first name with regex

--- registration regex/registration regex/Program.cs
+++ registration regex/registration regex/Program.cs
@@ -7,6 +7,10 @@ namespace registration_regex
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to my registration form regex apllication by Amit Rana");
+            RegexValidation regexValidation = new RegexValidation();
+            Console.Write("Enter Your First Name::-");
+            string FirstName = Console.ReadLine();
+            Console.WriteLine(regexValidation.ValidateFirstName(FirstName));
         }
     }
 }
--- registration regex/registration regex/RegexValidation.cs
+++ registration regex/registration regex/RegexValidation.cs
@@ -7,6 +7,10 @@ namespace registration_regex
 {
     class RegexValidation
     {
-        
+        public const string Regex_FirstName = "^[A-Z]{1}[A-Za-z]{2,}$";
+        public bool ValidateFirstName(string FirstName)
+        {
+            return Regex.IsMatch(FirstName, Regex_FirstName);
+        }
     }
 }
